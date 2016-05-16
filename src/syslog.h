c25284859fa96d4d2b8bd5832e6c44425091c3fe
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2016-05-16 08:23:15 +0000

    Add syslog for print message

---
+++ src/syslog.h
@@ -0,0 +1,35 @@
+/*! @file
+  @brief
+
+
+  <pre>
+  Copyright (C) 2015 Kyushu Institute of Technology.
+  Copyright (C) 2015 Shimane IT Open-Innovation Center.
+
+  This file is distributed under BSD 3-Clause License.
+
+
+  </pre>
+*/
+
+#ifndef MRUBYC_SRC_SYSLOG_H_
+#define MRUBYC_SRC_SYSLOG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
