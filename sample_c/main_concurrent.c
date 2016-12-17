6347f3de0b1e1f13a16a4b9115e5ea6dce905fa1
Hirohito Higashi <uzuki.local>
2016-12-18 00:46:53 +0900

    Change for using rrt0.

--- sample_c/main_concurrent.c
+++ sample_c/main_concurrent.c
@@ -1,57 +1,59 @@
 /*
  * Sample Main Program
+ *  Multitask version. (using mruby/c scheduler)
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "vm.h"
-#include "load.h"
-#include "errorcode.h"
-#include "static.h"
-#include "alloc.h"
+#include "rrt0.h"
+
+
+uint8_t * load_to_memory( const char *filename )
+{
+  FILE *fp = fopen(filename, "rb");
+
+  if( fp == NULL ) {
+    fprintf(stderr, "File not found\n");
+    return NULL;
+  }
+
+  // get filesize
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  fseek(fp, 0, SEEK_SET);
+
+  // allocate memory
+  uint8_t *p = malloc(size);
+  if( p == NULL ) {
+    fprintf(stderr, "Memory allocate error.\n");
+    return NULL;
+  }
+  fread(p, sizeof(uint8_t), size, fp);
+  fclose(fp);
+
+  return p;
+}
+
+
 
 int main(int argc, char *argv[])
 {
-  struct VM *vm[10];
-
-  mrbc_init_alloc();
-  init_static();
+  mrbc_init();
 
   int vm_cnt = argc-1;
   if( vm_cnt < 1 || vm_cnt > 10 ){
     printf("Usage: %s <xxxx.mrb> <xxxx.mrb> ... \n", argv[0]);
-    return -1;
+    return 1;
   }
 
   int i;
   for( i=0 ; i<vm_cnt ; i++ ){
-    vm[i] = vm_open();
-    if( vm[i] == 0 ){
-      printf("VM[%d] open Error\n", i);
-      return -1;
-    }
-    int ret = load_mrb_file(vm[i], argv[i+1]);
-    if( ret != NO_ERROR ){
-      printf("MRB Load Error (%04x_%04x)\n", ret>>16, ret&0xffff);
-      return -1;
-    }
-    vm_boot( vm[i] );
-  }
+    uint8_t *p = load_to_memory( argv[i+1] );
+    if( p == NULL ) return 1;
 
-
-  int keep_execute = 1;
-  while( keep_execute ){
-    keep_execute = 0;
-    for( i=0 ; i<vm_cnt ; i++ ){
-      if( vm[i] == 0 ) continue;
-      if( vm_run_step(vm[i]) < 0 ){
-        vm_close( vm[i] );
-        vm[i] = 0;
-      } else {
-        keep_execute = 1;
-      }
-    }
+    mrbc_create_task( p, 0 );
   }
+  mrbc_run();
 
   return 0;
 }
