68d268662d359f8ef513d51873b28779d2ef6e11
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2017-08-16 05:06:12 +0000

    op_move をリファレンスカウントに対応させた

--- src/vm.c
+++ src/vm.c
@@ -115,7 +115,7 @@ inline static int op_nop( mrb_vm *vm, uint32_t code, mrb_value *regs )
 */
 inline static int op_move( mrb_vm *vm, uint32_t code, mrb_value *regs )
 {
-  
+  mrbc_release(vm, &regs[GETARG_A(code)]);
   regs[GETARG_A(code)] = regs[GETARG_B(code)];
   return 0;
 }
