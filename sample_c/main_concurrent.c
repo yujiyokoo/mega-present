06a8959228a8add456ba5edb7dc9d9ae79448542
mimaki <hiroshi.mimaki@gmail.com>
2017-11-22 11:09:33 +0900

    Fixed memory leak on sample_c/mrubyc_concurrent

--- sample_c/main_concurrent.c
+++ sample_c/main_concurrent.c
@@ -10,12 +10,14 @@
 #define MEMORY_SIZE (1024*30)
 static uint8_t memory_pool[MEMORY_SIZE];
 
+#define MAX_APPL 10
+
 uint8_t * load_mrb_file(const char *filename)
 {
   FILE *fp = fopen(filename, "rb");
 
   if( fp == NULL ) {
-    fprintf(stderr, "File not found\n");
+    fprintf(stderr, "File not found (%s)\n", filename);
     return NULL;
   }
 
@@ -26,11 +28,11 @@ uint8_t * load_mrb_file(const char *filename)
 
   // allocate memory
   uint8_t *p = malloc(size);
-  if( p == NULL ) {
+  if( p != NULL ) {
+    fread(p, sizeof(uint8_t), size, fp);
+  } else {
     fprintf(stderr, "Memory allocate error.\n");
-    return NULL;
   }
-  fread(p, sizeof(uint8_t), size, fp);
   fclose(fp);
 
   return p;
@@ -40,25 +42,34 @@ uint8_t * load_mrb_file(const char *filename)
 
 int main(int argc, char *argv[])
 {
+  int i;
+  uint8_t *p[MAX_APPL] = {0};
+
   mrbc_init(memory_pool, MEMORY_SIZE);
 
   int vm_cnt = argc-1;
-  if( vm_cnt < 1 || vm_cnt > 10 ){
+  if( vm_cnt < 1 || vm_cnt > MAX_APPL ){
     printf("Usage: %s <xxxx.mrb> <xxxx.mrb> ... \n", argv[0]);
     return 1;
   }
 
-  int i;
   int flag_error = 0;
   for( i=0 ; i<vm_cnt ; i++ ){
-    uint8_t *p = load_mrb_file( argv[i+1] );
-    if( p == NULL ) return 1;
+    p[i] = load_mrb_file( argv[i+1] );
+    if( p[i] == NULL ){
+      flag_error = 1;
+      break;
+    }
 
-    if( mrbc_create_task( p, 0 ) == NULL ) flag_error = 1;
+    if( mrbc_create_task( p[i], 0 ) == NULL ) flag_error = 1;
   }
 
   if( !flag_error ) {
     mrbc_run();
+  }
+
+  for ( i=0 ; i<vm_cnt ; i++ ){
+    if ( p[i] != NULL ) free( p[i] );
   }
 
   return 0;
