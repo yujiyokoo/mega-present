f9812b22a1978e2bde92959940aee16e7695ffe1
Hirohito Higashi <uzuki.local>
2018-01-08 20:05:14 +0900

    append all header files.

--- src/mrubyc.h
+++ src/mrubyc.h
@@ -14,13 +14,26 @@
 #ifndef MRBC_SRC_MRUBYC_H_
 #define MRBC_SRC_MRUBYC_H_
 
+#include "vm_config.h"
+
 #include "errorcode.h"
 #include "vm.h"
 #include "value.h"
 #include "static.h"
 #include "alloc.h"
+#include "global.h"
+#include "symbol.h"
 #include "class.h"
+#include "c_array.h"
+#include "c_ext.h"
+#include "c_hash.h"
+#include "c_numeric.h"
+#include "c_range.h"
+#include "c_string.h"
+#include "c_symbol.h"
+
 #include "load.h"
+#include "console.h"
 #include "rrt0.h"
 
 #endif
