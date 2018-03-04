99d540fd9922d1d7848f6cbd1cca4d6abb658108
zurachu <zurachu@gmail.com>
2018-03-04 23:55:45 +0900

    fix bin_to_uint32() and bin_to_uint16()
    1. When address not aligned 32-bit is specified at environment requiring 32-bit address alignment(I tried at EPSON S1C33), address error exception is caused.
    2. Original source code is a simple swap, so it is not able to resolve the endianness dependency(perhaps the value is wrong in the big endianness environment
    ).

--- src/vm.h
+++ src/vm.h
@@ -102,8 +102,15 @@ int mrbc_vm_run(mrb_vm *vm);
 */
 inline static uint32_t bin_to_uint32( const void *s )
 {
-  uint32_t x = *((uint32_t *)s);
-  return (x << 24) | ((x & 0xff00) << 8) | ((x >> 8) & 0xff00) | (x >> 24);
+  uint8_t *p = (uint8_t *)s;
+  uint32_t x = *p++;
+  x <<= 8;
+  x |= *p++;
+  x <<= 8;
+  x |= *p++;
+  x <<= 8;
+  x |= *p;
+  return x;
 }
 
 
@@ -116,8 +123,10 @@ inline static uint32_t bin_to_uint32( const void *s )
 */
 inline static uint16_t bin_to_uint16( const void *s )
 {
-  uint16_t x = *((uint16_t *)s);
-  return (x << 8) | (x >> 8);
+  uint8_t *p = (uint8_t *)s;
+  uint16_t x = *p++ << 8;
+  x |= *p;
+  return x;
 }
 
 
