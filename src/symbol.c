e0806d7090b3feeabc194ac21780912a48802bdf
Hirohito Higashi <uzuki.local>
2018-04-07 13:02:35 +0900

    apply binary tree search to symbol.

--- src/symbol.c
+++ src/symbol.c
@@ -23,13 +23,25 @@
 #include "class.h"
 #include "symbol.h"
 #include "c_string.h"
+#include "c_array.h"
 #include "console.h"
 
 
+#if !defined(MRBC_SYMBOL_SEARCH_LINER) && !defined(MRBC_SYMBOL_SEARCH_BTREE)
+#define MRBC_SYMBOL_SEARCH_BTREE
+#endif
+
+#ifndef MRBC_SYMBOL_TABLE_INDEX_TYPE
+#define MRBC_SYMBOL_TABLE_INDEX_TYPE	uint16_t
+#endif
 
 struct SYM_INDEX {
   uint16_t hash;	//!< hash value, returned by calc_hash().
-  const char *pos;	//!< point to the symbol string.
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+  MRBC_SYMBOL_TABLE_INDEX_TYPE left;
+  MRBC_SYMBOL_TABLE_INDEX_TYPE right;
+#endif
+  const char *cstr;	//!< point to the symbol string.
 };
 
 
@@ -42,15 +54,72 @@ static int sym_index_pos;	// point to the last(free) sym_index array.
  */
 static int search_index( uint16_t hash, const char *str )
 {
+#ifdef MRBC_SYMBOL_SEARCH_LINER
   int i;
-
   for( i = 0; i < sym_index_pos; i++ ) {
-    if( sym_index[i].hash == hash && strcmp(str, sym_index[i].pos) == 0 ) {
+    if( sym_index[i].hash == hash && strcmp(str, sym_index[i].cstr) == 0 ) {
       return i;
     }
   }
-
   return -1;
+#endif
+
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+  int i = 0;
+  do {
+    if( sym_index[i].hash == hash && strcmp(str, sym_index[i].cstr) == 0 ) {
+      return i;
+    }
+    if( hash < sym_index[i].hash ) {
+      i = sym_index[i].left;
+    } else {
+      i = sym_index[i].right;
+    }
+  } while( i != 0 );
+  return -1;
+#endif
+}
+
+
+//================================================================
+/*! add to index table
+ */
+static int add_index( uint16_t hash, const char *str )
+{
+  // check overflow.
+  if( sym_index_pos >= MAX_SYMBOLS_COUNT ) {
+    console_printf( "Overflow %s for '%s'\n", "MAX_SYMBOLS_COUNT", str );
+    return -1;
+  }
+
+  int sym_id = sym_index_pos++;
+
+  // append table.
+  sym_index[sym_id].hash = hash;
+  sym_index[sym_id].cstr = str;
+
+#ifdef MRBC_SYMBOL_SEARCH_BTREE
+  int i = 0;
+
+  while( 1 ) {
+    if( hash < sym_index[i].hash ) {
+      // left side
+      if( sym_index[i].left == 0 ) {	// left is empty?
+        sym_index[i].left = sym_id;
+        break;
+      }
+      i = sym_index[i].left;
+    } else {
+      // right side
+      if( sym_index[i].right == 0 ) {	// right is empty?
+        sym_index[i].right = sym_id;
+        break;
+      }
+      i = sym_index[i].right;
+    }
+  }
+#endif
+  return sym_id;
 }
 
 
@@ -72,22 +141,13 @@ mrb_value mrbc_symbol_new(struct VM *vm, const char *str)
     return ret;		// already exist.
   }
 
-  // check overflow.
-  if( sym_index_pos >= MAX_SYMBOLS_COUNT ) {
-    console_printf( "Overflow %s for '%s'\n", "MAX_SYMBOLS_COUNT", str );
-    return ret;
-  }
-
   // create symbol object dynamically.
   int size = strlen(str) + 1;
   char *buf = mrbc_raw_alloc(size);
   if( buf == NULL ) return ret;		// ENOMEM raise?
 
   memcpy(buf, str, size);
-
-  sym_index[sym_index_pos].hash = h;
-  sym_index[sym_index_pos].pos = buf;
-  ret.i = sym_index_pos++;
+  ret.i = add_index( h, buf );
 
   return ret;
 }
@@ -123,17 +183,7 @@ mrb_sym str_to_symid(const char *str)
   mrb_sym sym_id = search_index(h, str);
   if( sym_id >= 0 ) return sym_id;
 
-  // check overflow.
-  if( sym_index_pos >= MAX_SYMBOLS_COUNT ) {
-    console_printf( "Overflow %s '%s'\n", "MAX_SYMBOLS_COUNT", str );
-    return -1;
-  }
-
-  // append index table.
-  sym_index[sym_index_pos].hash = h;
-  sym_index[sym_index_pos].pos = str;
-
-  return sym_index_pos++;
+  return add_index( h, str );
 }
 
 
@@ -149,9 +199,26 @@ const char * symid_to_str(mrb_sym sym_id)
   if( sym_id < 0 ) return NULL;
   if( sym_id >= sym_index_pos ) return NULL;
 
-  return sym_index[sym_id].pos;
+  return sym_index[sym_id].cstr;
 }
 
+
+
+//================================================================
+/*! (method) all_symbols
+*/
+static void c_all_symbols(mrb_vm *vm, mrb_value v[], int argc)
+{
+  mrb_value ret = mrbc_array_new(vm, sym_index_pos);
+
+  int i;
+  for( i = 0; i < sym_index_pos; i++ ) {
+    mrb_value sym1 = {.tt = MRB_TT_SYMBOL};
+    sym1.i = i;
+    mrbc_array_push(&ret, &sym1);
+  }
+  SET_RETURN(ret);
+}
 
 
 #if MRBC_USE_STRING
@@ -180,11 +247,14 @@ static void c_equal3(mrb_vm *vm, mrb_value v[], int argc)
 }
 
 
-
+//================================================================
+/*! initialize
+*/
 void mrbc_init_class_symbol(struct VM *vm)
 {
   mrbc_class_symbol = mrbc_define_class(vm, "Symbol", mrbc_class_object);
 
+  mrbc_define_method(vm, mrbc_class_symbol, "all_symbols", c_all_symbols);
 #if MRBC_USE_STRING
   mrbc_define_method(vm, mrbc_class_symbol, "to_s", c_to_s);
   mrbc_define_method(vm, mrbc_class_symbol, "id2name", c_to_s);
