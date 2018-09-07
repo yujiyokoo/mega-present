32353b620273d3f7a1410a760128c618720e6e66
Hirohito Higashi <uzuki.local>
2018-09-07 12:58:43 +0900

    Implement String#tr, tr!, start_with?, end_with? and include? method.

--- src/c_string.c
+++ src/c_string.c
@@ -14,6 +14,7 @@
 #include "vm_config.h"
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 #include <assert.h>
 
 #include "value.h"
@@ -1009,6 +1010,256 @@ static void c_string_to_sym(struct VM *vm, mrbc_value v[], int argc)
 }
 
 
+//================================================================
+/*! (method) tr
+
+  Pattern syntax
+
+  <syntax> ::= (<pattern>)* | '^' (<pattern>)* | '^'
+  <pattern> ::= <in order> | <range>
+  <in order> ::= (<ch>)+
+  <range> ::= <ch> '-' <ch>
+*/
+struct tr_pattern {
+  uint8_t type;		// 1:in-order, 2:range
+  uint8_t flag_reverse;
+  int16_t n;
+  struct tr_pattern *next;
+  char ch[];
+};
+
+static struct tr_pattern * tr_parse_pattern( struct VM *vm, const mrb_value *v_pattern, int flag_reverse_enable )
+{
+  const char *pattern = mrbc_string_cstr( v_pattern );
+  int pattern_length = mrbc_string_size( v_pattern );
+  int flag_reverse = 0;
+  struct tr_pattern *ret = NULL;
+
+  int i = 0;
+  if( flag_reverse_enable && pattern[i] == '^' ) {
+    flag_reverse = 1;
+
+    if( ++i == pattern_length ) {		// only "^" pattern
+      ret = mrbc_alloc( vm, sizeof(struct tr_pattern) + 1 );
+      if( ret != NULL ) {
+	ret->type = 1;
+	ret->flag_reverse = 0;
+	ret->n = 1;
+	ret->next = NULL;
+	ret->ch[0] = '^';
+      }
+      return ret;
+    }
+  }
+
+  struct tr_pattern *pat1;
+  while( i < pattern_length ) {
+    // is range pattern ?
+    if( (i+2) < pattern_length && pattern[i+1] == '-' ) {
+      pat1 = mrbc_alloc( vm, sizeof(struct tr_pattern) + 2 );
+      if( pat1 != NULL ) {
+	pat1->type = 2;
+	pat1->flag_reverse = flag_reverse;
+	pat1->n = pattern[i+2] - pattern[i] + 1;
+	pat1->next = NULL;
+	pat1->ch[0] = pattern[i];
+	pat1->ch[1] = pattern[i+2];
+      }
+      i += 3;
+
+    } else {
+      // in order pattern.
+      int start_pos = i++;
+      while( i < pattern_length ) {
+	if( (i+2) < pattern_length && pattern[i+1] == '-' ) break;
+	i++;
+      }
+
+      int len = i - start_pos;
+      pat1 = mrbc_alloc( vm, sizeof(struct tr_pattern) + len );
+      if( pat1 != NULL ) {
+	pat1->type = 1;
+	pat1->flag_reverse = flag_reverse;
+	pat1->n = len;
+	pat1->next = NULL;
+	memcpy( pat1->ch, &pattern[start_pos], len );
+      }
+    }
+
+    if( ret == NULL ) {
+      ret = pat1;
+    } else {
+      struct tr_pattern *p = ret;
+      while( p->next != NULL ) { p = p->next; }
+      p->next = pat1;
+    }
+  }
+
+  return ret;
+}
+
+static int tr_find_character( const struct tr_pattern *pat, int ch )
+{
+  int ret = -1;
+  int n_sum = 0;
+  int flag_reverse = pat ? pat->flag_reverse : 0;
+
+  while( pat != NULL ) {
+    if( pat->type == 1 ) {	// in-order
+      int i;
+      for( i = 0; i < pat->n; i++ ) {
+	if( pat->ch[i] == ch ) ret = n_sum + i;
+      }
+    } else {	// pat->type == 2  range
+      if( pat->ch[0] <= ch && ch <= pat->ch[1] ) ret = n_sum + ch - pat->ch[0];
+    }
+    n_sum += pat->n;
+    pat = pat->next;
+  }
+
+  if( flag_reverse ) {
+    return (ret < 0) ? INT_MAX : -1;
+  }
+  return ret;
+}
+
+static int tr_get_character( const struct tr_pattern *pat, int n_th )
+{
+  int n_sum = 0;
+  while( pat != NULL ) {
+    if( n_th < (n_sum + pat->n) ) {
+      int i = (n_th - n_sum);
+      return (pat->type == 1) ? pat->ch[i] :pat->ch[0] + i;
+    }
+    if( pat->next == NULL ) {
+      return (pat->type == 1) ? pat->ch[pat->n - 1] : pat->ch[1];
+    }
+    n_sum += pat->n;
+    pat = pat->next;
+  }
+
+  return -1;
+}
+
+static int tr_main( struct VM *vm, mrbc_value v[], int argc )
+{
+  if( !(argc == 2 && v[1].tt == MRBC_TT_STRING && v[2].tt == MRBC_TT_STRING)) {
+    console_print("ArgumentError\n");	// raise?
+    return -1;
+  }
+
+  struct tr_pattern *pat = tr_parse_pattern( vm, &v[1], 1 );
+  if( pat == NULL ) return 0;
+
+  struct tr_pattern *rep = tr_parse_pattern( vm, &v[2], 0 );
+
+  int flag_changed = 0;
+  char *s = mrbc_string_cstr( &v[0] );
+  int len = mrbc_string_size( &v[0] );
+  int i;
+  for( i = 0; i < len; i++ ) {
+    int n = tr_find_character( pat, s[i] );
+    if( n >= 0 ) {
+      flag_changed = 1;
+      if( rep == NULL ) {
+	memmove( s + i, s + i + 1, len - i );
+	len--;
+	i--;
+      } else {
+	s[i] = tr_get_character( rep, n );
+      }
+    }
+  }
+
+  mrbc_raw_free( pat );
+  if( rep ) mrbc_raw_free( rep );
+
+  v[0].string->size = len;
+  v[0].string->data[len] = 0;
+
+  return flag_changed;
+}
+
+static void c_string_tr(struct VM *vm, mrbc_value v[], int argc)
+{
+  mrb_value ret = mrbc_string_dup( vm, &v[0] );
+  SET_RETURN( ret );
+  tr_main(vm, v, argc);
+}
+
+
+//================================================================
+/*! (method) tr!
+*/
+static void c_string_tr_self(struct VM *vm, mrbc_value v[], int argc)
+{
+  int flag_changed = tr_main(vm, v, argc);
+
+  if( !flag_changed ) {
+    SET_NIL_RETURN();
+  }
+}
+
+
+//================================================================
+/*! (method) start_with?
+*/
+static void c_string_start_with(struct VM *vm, mrbc_value v[], int argc)
+{
+  if( !(argc == 1 && v[1].tt == MRBC_TT_STRING)) {
+    console_print("ArgumentError\n");	// raise?
+    return;
+  }
+
+  int ret;
+  if( mrbc_string_size(&v[0]) < mrbc_string_size(&v[1]) ) {
+    ret = 0;
+  } else {
+    ret = (memcmp( mrbc_string_cstr(&v[0]), mrbc_string_cstr(&v[1]),
+		   mrbc_string_size(&v[1]) ) == 0);
+  }
+
+  SET_BOOL_RETURN(ret);
+}
+
+
+//================================================================
+/*! (method) end_with?
+*/
+static void c_string_end_with(struct VM *vm, mrbc_value v[], int argc)
+{
+  if( !(argc == 1 && v[1].tt == MRBC_TT_STRING)) {
+    console_print("ArgumentError\n");	// raise?
+    return;
+  }
+
+  int ret;
+  int offset = mrbc_string_size(&v[0]) - mrbc_string_size(&v[1]);
+  if( offset < 0 ) {
+    ret = 0;
+  } else {
+    ret = (memcmp( mrbc_string_cstr(&v[0]) + offset, mrbc_string_cstr(&v[1]),
+		   mrbc_string_size(&v[1]) ) == 0);
+  }
+
+  SET_BOOL_RETURN(ret);
+}
+
+
+//================================================================
+/*! (method) include?
+*/
+static void c_string_include(struct VM *vm, mrbc_value v[], int argc)
+{
+  if( !(argc == 1 && v[1].tt == MRBC_TT_STRING)) {
+    console_print("ArgumentError\n");	// raise?
+    return;
+  }
+
+  int ret = mrbc_string_index( &v[0], &v[1], 0 );
+  SET_BOOL_RETURN(ret >= 0);
+}
+
 
 //================================================================
 /*! initialize
@@ -1041,6 +1292,11 @@ void mrbc_init_class_string(struct VM *vm)
   mrbc_define_method(vm, mrbc_class_string, "strip!",	c_string_strip_self);
   mrbc_define_method(vm, mrbc_class_string, "to_sym",	c_string_to_sym);
   mrbc_define_method(vm, mrbc_class_string, "intern",	c_string_to_sym);
+  mrbc_define_method(vm, mrbc_class_string, "tr",	c_string_tr);
+  mrbc_define_method(vm, mrbc_class_string, "tr!",	c_string_tr_self);
+  mrbc_define_method(vm, mrbc_class_string, "start_with?", c_string_start_with);
+  mrbc_define_method(vm, mrbc_class_string, "end_with?", c_string_end_with);
+  mrbc_define_method(vm, mrbc_class_string, "include?",	c_string_include);
 
 #if MRBC_USE_FLOAT
   mrbc_define_method(vm, mrbc_class_string, "to_f",	c_string_to_f);
