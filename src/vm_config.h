d2e18463a9b86f6bfca90e479e6b5ad09e6cdc1d
Hirohito Higashi <uzuki.local>
2019-04-01 16:07:16 +0900

    Refactoring build (make) system.

--- src/vm_config.h
+++ src/vm_config.h
@@ -3,8 +3,8 @@
   Global configration of mruby/c VM's
 
   <pre>
-  Copyright (C) 2015 Kyushu Institute of Technology.
-  Copyright (C) 2015 Shimane IT Open-Innovation Center.
+  Copyright (C) 2015-2019 Kyushu Institute of Technology.
+  Copyright (C) 2015-2019 Shimane IT Open-Innovation Center.
 
   This file is distributed under BSD 3-Clause License.
 
@@ -15,60 +15,49 @@
 #ifndef MRBC_SRC_VM_CONFIG_H_
 #define MRBC_SRC_VM_CONFIG_H_
 
-/* maximum number of VMs */
-#ifndef MAX_VM_COUNT
+// maximum number of VMs
+#if !defined(MAX_VM_COUNT)
 #define MAX_VM_COUNT 5
 #endif
 
-/* maximum size of registers */
-#ifndef MAX_REGS_SIZE
+// maximum size of registers
+#if !defined(MAX_REGS_SIZE)
 #define MAX_REGS_SIZE 100
 #endif
 
-/* maximum size of callinfo (callstack) */
-#ifndef MAX_CALLINFO_SIZE
-#define MAX_CALLINFO_SIZE 100
-#endif
-
-/* maximum number of objects */
-#ifndef MAX_OBJECT_COUNT
-#define MAX_OBJECT_COUNT 400
-#endif
-
-/* maximum number of classes */
-#ifndef MAX_CLASS_COUNT
-#define MAX_CLASS_COUNT 20
-#endif
-
-/* maximum number of symbols */
-#ifndef MAX_SYMBOLS_COUNT
+// maximum number of symbols
+#if !defined(MAX_SYMBOLS_COUNT)
 #define MAX_SYMBOLS_COUNT 300
 #endif
 
-/* maximum size of global objects */
-#ifndef MAX_GLOBAL_OBJECT_SIZE
-#define MAX_GLOBAL_OBJECT_SIZE 50
-#endif
 
-/* maximum size of consts */
-#ifndef MAX_CONST_COUNT
-#define MAX_CONST_COUNT 20
-#endif
-
-
-/* Configure environment */
-/* 0: NOT USE */
-/* 1: USE */
-
-/* USE Float. Support Float class */
+/* Configure environment
+   0: NOT USE
+   1: USE
+*/
+// USE Float. Support Float class.
+#if !defined(MRBC_USE_FLOAT)
 #define MRBC_USE_FLOAT 1
+#endif
 
-/* USE Math class */
+// Use math. Support Math class.
+#if !defined(MRBC_USE_MATH)
 #define MRBC_USE_MATH 0
+#endif
+/* (NOTE)
+   maybe you need
+   $ export LDFLAGS=-lm
+   $ make
 
-/* USE String. Support String class */
+   on utuntu
+   $ export LDFAGS="-Wl,--no-as-needed -lm"
+   $ make
+*/
+
+// USE String. Support String class.
+#if !defined(MRBC_USE_STRING)
 #define MRBC_USE_STRING 1
-
+#endif
 
 
 /* Hardware dependent flags */
@@ -84,5 +73,11 @@
    If 32-bit alignment is required, enable the following line.
  */
 // #define MRBC_REQUIRE_32BIT_ALIGNMENT
+
+// Debug code.
+#if !defined(MRBC_DEBUG)
+#define MRBC_DEBUG
+#endif
+
 
 #endif
