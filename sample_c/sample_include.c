4ca2b1daf6b65a8ba065854b9375cf76e1f12113
Kazuaki Tanaka <kazuaki@mse.kyutech.ac.jp>
2019-07-28 17:53:11 +0900

    Change sample program name

--- sample_c/sample_include.c
+++ sample_c/sample_include.c
@@ -1,12 +1,16 @@
 /*
- * Sample Main Program
+ * This sample program executes a static mruby/c program.
+ * The compiled mruby bytecode file should be prepared by "mrbc" command as followings:
+ *
+ *    mrbc -E -Bary sample_include_bytecode.rb
+ *
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "mrubyc.h"
 
-#include "sample01.c"
+#include "sample_include_bytecode.c"
 
 #define MEMORY_SIZE (1024*30)
 static uint8_t memory_pool[MEMORY_SIZE];
