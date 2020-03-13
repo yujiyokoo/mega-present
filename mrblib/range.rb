5db79a07b5a2d89dccd2eb418a5266a0450e866f
Hirohito Higashi <uzuki.local>
2020-03-13 23:45:09 +0900

    refactoring.

--- mrblib/range.rb
+++ mrblib/range.rb
@@ -11,12 +11,9 @@ class Range
 
   # each
   def each
-    val = self.first
-    last = self.last
-
-    lim = last
+    lim = self.last
     lim += 1 unless exclude_end?
-    i = val
+    i = self.first
     while i < lim do
       yield i
       i += 1
