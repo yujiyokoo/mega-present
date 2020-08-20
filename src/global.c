65bc92a02de2b8190ab6ab26970e264d3837838b
HirohitoHigashi <higashi@s-itoc.jp>
2020-08-20 23:55:56 +0900

    change initial allocation size of constant table.

--- src/global.c
+++ src/global.c
@@ -29,7 +29,7 @@ static mrbc_kv_handle handle_global;	//!< for global variables.
 */
 void mrbc_init_global(void)
 {
-  mrbc_kv_init_handle( 0, &handle_const, 15 );
+  mrbc_kv_init_handle( 0, &handle_const, 25 );
   mrbc_kv_init_handle( 0, &handle_global, 0 );
 }
 
