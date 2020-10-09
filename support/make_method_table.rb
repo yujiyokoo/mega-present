29706d69ae418e01dd88d7d4427614940123274c
HirohitoHigashi <higashi@s-itoc.jp>
2020-10-09 15:39:45 +0900

    using common_sub.rb

--- support/make_method_table.rb
+++ support/make_method_table.rb
@@ -12,141 +12,8 @@
 # ruby make_method_table.rb TARGET_CLASS_FILE.c
 #
 
-require_relative "rename_for_symbol"
+require_relative "common_sub"
 
-##
-# strip double quot
-#
-def strip_double_quot( s )
-  ret = s.dup
-  ret.slice!(0)  if ret[0] == '"'
-  ret.chop! if ret[-1] == '"'
-
-  return ret
-end
-
-
-#
-# find and get source code.
-#
-def get_method_table_source( file )
-  ret = nil
-
-  # find magic comment
-  while txt = file.gets
-    if /\/\*\s+MRBC_AUTOGEN_METHOD_TABLE/ =~ txt
-      ret = ""
-      break
-    end
-  end
-  return nil  if !ret
-
-  while txt = file.gets
-    # skip comment and empty line.
-    txt = txt.split("//", 2)[0].strip
-    next if txt.empty?
-
-    break  if txt.start_with?("*/")
-    ret << txt << "\n"
-  end
-
-  return ret
-end
-
-
-##
-# parser
-#
-# (note)
-#  create hash, below.
-# {
-#   class: "Range",
-#   file: "c_range_method_table.h",
-#   func: "mrbc_init_class_range",
-#   super: "mrbc_class_object",
-#   methods: [
-#     { name: "first", func: "c_range_first", if_exp: "" }
-#       ...
-#   ]
-# }
-#
-def parse_source_string( src )
-  flag_error = false
-  ret = { methods:[] }
-  if_exp = []
-  src.each_line {|txt|
-    # e.g. "#if ..."
-    if /^#\s*if/ =~ txt
-      if_exp << txt
-      next
-    end
-    if /^#\s*endif/ =~ txt
-      if_exp.pop
-      next
-    end
-
-    # e.g. CLASS, METHOD and etc.
-    if /^([A-Z]+)\s*\((.*)\)/ =~ txt
-      key = $1
-      args = $2.split(",").map {|s| s.strip }
-      flag_arg_ok = true
-      case key
-      when "CLASS", "FILE", "FUNC", "SUPER"
-        if args.size == 1
-          ret[key.downcase.to_sym] = args[0]
-        else
-          flag_arg_ok = false
-        end
-
-      when "METHOD"
-        if args.size == 2
-          ret[:methods] << { name: strip_double_quot(args[0]), func: args[1] }
-          ret[:methods].last[:if_exp] = if_exp.dup  if !if_exp.empty?
-        else
-          flag_arg_ok = false
-        end
-
-      else
-        puts "Error: Invalid keyword. '#{key}'"
-      end
-
-      if !flag_arg_ok
-        puts "Error: argument error. #{txt}"
-      end
-      next
-    end
-
-    puts "Error: #{txt}"
-    flag_error = true
-  }
-
-  ret[:super] ||= "mrbc_class_object"
-
-  return flag_error ? nil : ret
-end
-
-
-##
-# error check
-#
-def check_error( param )
-  flag_error = false
-
-  if !param[:class]
-    puts "Error: 'CLASS' parameter required"
-    flag_error = true
-  end
-  if !param[:file]
-    puts "Error: 'FILE' parameter required"
-    flag_error = true
-  end
-  if !param[:func]
-    puts "Error: 'FUNC' parameter required"
-    flag_error = true
-  end
-
-  return !flag_error
-end
 
 
 ##
@@ -185,7 +52,7 @@ def output_header_file( param )
   file.puts "  };"
   file.puts
 
-  file.puts "  return mrbc_define_builtin_class(#{param[:class]}, #{param[:super]}, method_symbols, method_functions, sizeof(method_symbols)/sizeof(mrbc_sym) );"
+  file.puts "  return mrbc_define_builtin_class(\"#{param[:class]}\", #{param[:super]}, method_symbols, method_functions, sizeof(method_symbols)/sizeof(mrbc_sym) );"
   file.puts "}"
 
   file.close
