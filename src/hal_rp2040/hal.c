b00aa1e29bbab2bb585b2051078fb642cb636d1d
aikawaYO <makonoku@yahoo.co.jp>
2021-03-23 21:10:35 +0900

    Revert "delete hal method rp2040"
    This reverts commit 89c5668761a823d4eaef131e6bed90c5d9666df6.

--- src/hal_rp2040/hal.c
+++ src/hal_rp2040/hal.c
@@ -49,4 +49,27 @@ void hal_init(void){
     clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS | CLOCKS_SLEEP_EN1_CLK_SYS_USBCTRL_BITS | CLOCKS_SLEEP_EN1_CLK_USB_USBCTRL_BITS;
 }
 
+//================================================================
+/*!@brief
+  Write
+
+  @param  fd    dummy, but 1.
+  @param  buf   pointer of buffer.
+  @param  nbytes        output byte length.
+*/
+int hal_write(int fd, const void *buf, int nbytes){
+    printf(buf);
+    return nbytes;
+}
+
+//================================================================
+/*!@brief
+  Flush write baffer
+
+  @param  fd    dummy, but 1.
+*/
+int hal_flush(int fd) {
+    return 0;
+}
+
 #endif /* ifndef MRBC_NO_TIMER */
