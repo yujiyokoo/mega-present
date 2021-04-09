22193594b6284dc74a1ee7a1da6322ed1810e97f
aikawaYO <makonoku@yahoo.co.jp>
2021-04-09 22:08:04 +0900

    align indent rp2040 hal.c

--- src/hal_rp2040/hal.c
+++ src/hal_rp2040/hal.c
@@ -35,7 +35,7 @@ struct repeating_timer timer;
 
 */
 bool alarm_irq(struct repeating_timer *t) {
-    mrbc_tick();
+  mrbc_tick();
 }
 
 //================================================================
@@ -44,13 +44,13 @@ bool alarm_irq(struct repeating_timer *t) {
 
 */
 void hal_init(void){
-    add_repeating_timer_ms(1, alarm_irq, NULL, &timer);
-    clocks_hw->sleep_en0 = 0;
-    clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS 
-                          | CLOCKS_SLEEP_EN1_CLK_SYS_USBCTRL_BITS
-                          | CLOCKS_SLEEP_EN1_CLK_USB_USBCTRL_BITS
-                          | CLOCKS_SLEEP_EN1_CLK_SYS_UART0_BITS
-                          | CLOCKS_SLEEP_EN1_CLK_PERI_UART0_BITS;
+  add_repeating_timer_ms(1, alarm_irq, NULL, &timer);
+  clocks_hw->sleep_en0 = 0;
+  clocks_hw->sleep_en1 = CLOCKS_SLEEP_EN1_CLK_SYS_TIMER_BITS 
+                        | CLOCKS_SLEEP_EN1_CLK_SYS_USBCTRL_BITS
+                        | CLOCKS_SLEEP_EN1_CLK_USB_USBCTRL_BITS
+                        | CLOCKS_SLEEP_EN1_CLK_SYS_UART0_BITS
+                        | CLOCKS_SLEEP_EN1_CLK_PERI_UART0_BITS;
 }
 
 //================================================================
@@ -90,7 +90,7 @@ int hal_write(int fd, const void *buf, int nbytes)
   @param  fd    dummy, but 1.
 */
 int hal_flush(int fd) {
-    return 0;
+  return 0;
 }
 
 #endif /* ifndef MRBC_NO_TIMER */
