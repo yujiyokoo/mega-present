3953c4c3334cd882e8734e0a51e6c29ac016ebe6
HirohitoHigashi <higashi@s-itoc.jp>
2022-05-18 14:41:29 +0900

    add const.

--- src/global.h
+++ src/global.h
@@ -29,9 +29,9 @@ extern "C" {
 /***** Function prototypes **************************************************/
 void mrbc_init_global(void);
 int mrbc_set_const(mrbc_sym sym_id, mrbc_value *v);
-int mrbc_set_class_const(struct RClass *cls, mrbc_sym sym_id, mrbc_value *v);
+int mrbc_set_class_const(const struct RClass *cls, mrbc_sym sym_id, mrbc_value *v);
 mrbc_value *mrbc_get_const(mrbc_sym sym_id);
-mrbc_value *mrbc_get_class_const(struct RClass *cls, mrbc_sym sym_id);
+mrbc_value *mrbc_get_class_const(const struct RClass *cls, mrbc_sym sym_id);
 int mrbc_set_global(mrbc_sym sym_id, mrbc_value *v);
 mrbc_value *mrbc_get_global(mrbc_sym sym_id);
 void mrbc_global_clear_vm_id(void);
