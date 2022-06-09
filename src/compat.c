1e1ba5ab1d8e67a5de62074c42aef9bcaa16338d
Yuji Yokoo <yujiyokoo@gmail.com>
2022-06-09 22:15:40 +0930

    Remove unnecessary implementations

--- src/compat.c
+++ src/compat.c
@@ -7,31 +7,6 @@
 #define int8_t char
 #endif
 
-// copied from https://github.com/Stephane-D/SGDK/blob/d7d3d861391a8d3cbd4c029b9dca48088b688ae7/src/ext/mw/gamejolt.c
-// memcmp not available on SGDK
-static int8_t memcmp(const char *m1, const char *m2, int16_t len)
-{
-	int8_t dif = 0;
-	for (int16_t i = 0; i < len && !dif; i++) {
-		dif = m1[i] - m2[i];
-	}
-
-	return dif;
-}
-
-// strchr not available on SGDK
-static char *strchr(const char *s, int c)
-{
-	while (*s) {
-		if (*s == (char)c) {
-			return (char*)s;
-		}
-		s++;
-	}
-
-	return NULL;
-}
-
 // hal_write implementation for now...
 int hal_write(int fd, const void *buf, int nbytes) {
   static int8_t line = 20;
