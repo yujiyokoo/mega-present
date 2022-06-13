9a5ecff1d2365be03e8e636c8ff591f3d9f2130f
Yuji Yokoo <yujiyokoo@gmail.com>
2022-06-13 11:51:24 +0930

    Remove conflicting definition and update function signature

--- src/compat.h
+++ src/compat.h
@@ -1,9 +1,9 @@
 #ifndef __COMPAT_H_
 #define __COMPAT_H_
 
-#define size_t unsigned int
+// #define size_t unsigned int
 
-int memcmp (const void *, const void *, size_t);
+int memcmp (const void *, const void *, unsigned int);
 char *strchr(const char *, int);
 
 #endif // __COMPAT_H_
