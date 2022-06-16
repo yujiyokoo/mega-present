fd8b01710808b773f5b712103233b44238b29c3f
Yuji Yokoo <yujiyokoo@gmail.com>
2022-06-17 00:32:05 +0930

    Implement more game features

--- src/game.rb
+++ src/game.rb
@@ -1,36 +1,210 @@
 # MegaMrbc.draw_text("Hello, Megadrive from ruby/c")
 
+def draw_text(str, x, y)
+  MegaMrbc.draw_text(str, x, y)
+end
+
+def joypad_state(pad_num)
+  MegaMrbc.read_joypad(pad_num)
+end
+
+def wait_vblank
+  MegaMrbc.wait_vblank
+end
+
 class Game
   def main
     while true do
-      GameRound.game_loop
+      draw_text("press start", 0, 10)
+      wait_start
+      draw_text("           ", 0, 10)
+      GameRound.new.game_loop
+    end
+  end
+
+  def wait_start
+    state = 0
+    prev = 0
+    while true do
+      state = joypad_state(0)
+      break if (state & 0x80 & ~prev) != 0
+      wait_vblank
     end
   end
 end
 
 class GameRound
-  def self.game_loop
+  KEYBOARD = [
+    ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+    ["A", "S", "D", "F", "G", "H", "J", "K", "L", "<"],
+    ["Z", "X", "C", "V", "B", "N", "M", " ", "", ""]
+  ]
+
+  def game_loop
+    @answer = "MRUBY"
+    @curr_x = 0
+    @curr_y = 0
+    # current buffer is always 5 in length
+    @curr_buf = "     "
+    @curr_index = 0
+    draw_grid(12, 0)
+    draw_kb(5, 19)
+    draw_tick
+    
+    prev_state = 0
+    pad_state = 0
     running = true
     while running do
-      [0,1].each do |i|
-        pad_state = joypad_state(i)
-        draw_text("Joypad state: #{pad_state}    ", 4, 20+i)
+      # draw_text("#{@curr_x}, #{@curr_y}", 0, 0)
+      # draw_text("#{@curr_buf}", 0, 2)
+      MegaMrbc.show_cursor(@curr_x * 3 + 5, @curr_y * 3 + 19)
+      pad_state = joypad_state(0)
+      render_guess
+      # draw_text("#{pad_state}", 0, 1)
+      move_cursor(pad_state, prev_state)
+      won = accept_letter(pad_state, prev_state)
+      if won
+        # finish game
+        draw_text("YOU WIN!", 0, 4)
+        break
+      elsif @curr_index > 2 # 5
+        draw_text("GAME OVER!", 0, 3)
+        break
+      # game over
+      end
+      prev_state = pad_state
+      wait_vblank
+    end
+  end
 
-        wait_vblank
+  def accept_letter(state, prev)
+    if (state & 0x40 & ~prev) != 0
+      c = current_char(@curr_x, @curr_y)
+      len = @curr_buf.strip.length
+      if c == "<"
+        @curr_buf[len-1] = " " if len > 0
+      elsif c == " "
+        if len == 5
+          colourise_guess # unimplemented
+          return true if @curr_buf == @answer
+          reset_buf
+          @curr_index += 1
+        end
+      elsif len < 5
+        #draw_text("adding letter #{c}", 0, 6)
+        #draw_text("state, prev #{state}, #{prev}    ", 0, 7)
+        @curr_buf[len] = c
+      end
+    end
+    return false
+  end
+
+  def current_char(x, y)
+    KEYBOARD[y] && KEYBOARD[y][x]
+  end
+
+  def render_guess
+    [0, 1, 2, 3, 4].each do |i|
+      draw_text(@curr_buf[i], 13 + 3 * i, @curr_index * 3 + 1)
+    end
+  end
+
+  def colourise_guess
+    [0, 1, 2, 3, 4].each do |i|
+      if @curr_buf[i] == @answer[i]
+        # green
+        MegaMrbc.draw_green_square(13 + 3 * i, @curr_index * 3 + 1)
+      elsif @answer.include? @curr_buf[i]
+        # yellow
+        MegaMrbc.draw_yellow_square(13 + 3 * i, @curr_index * 3 + 1)
       end
     end
   end
 
-  def self.joypad_state(pad_num)
-    MegaMrbc.read_joypad(pad_num)
+  def reset_buf
+    [0, 1, 2, 3, 4].each do |i| 
+      @curr_buf[i] = " "
+    end
   end
 
-  def self.draw_text(str, x, y)
-    MegaMrbc.draw_text(str, x, y)
+  def move_cursor(state, prev)
+    case
+    when (state & 0x01 & ~prev) != 0
+      move_up
+    when (state & 0x04 & ~prev) != 0
+      move_left
+    when (state & 0x02 & ~prev) != 0
+      move_down
+    when (state & 0x08 & ~prev) != 0
+      move_right
+    else
+      # nothing
+    end
   end
 
-  def self.wait_vblank
-    MegaMrbc.wait_vblank
+  def valid_position?(x, y)
+    KEYBOARD[y] && KEYBOARD[y][x] && KEYBOARD[y][x] != ""
+  end
+
+  def move_up
+    new_y = @curr_y - 1
+    if new_y >= 0 && valid_position?(@curr_x, new_y)
+      @curr_y = new_y
+    end
+  end
+
+  def move_left
+    new_x = @curr_x - 1
+    if new_x >= 0 && valid_position?(new_x, @curr_y)
+      @curr_x = new_x
+    end
+  end
+
+  def move_down
+    new_y = @curr_y + 1
+    if new_y >= 0 && valid_position?(@curr_x, new_y)
+      @curr_y = new_y
+    end
+  end
+
+  def move_right
+    new_x = @curr_x + 1
+    if new_x >= 0 && valid_position?(new_x, @curr_y)
+      @curr_x = new_x
+    end
+  end
+
+  def draw_tick
+    MegaMrbc.show_tick(5 + 22, 19 + 7)
+  end
+
+  def draw_grid(x, y)
+    [0, 1, 2, 3, 4, 5].each do |i|
+      [0, 1, 2, 3, 4].each do |j|
+        draw_rect(x + j * 3, y + i * 3)
+      end
+    end
+  end
+
+  # draws a 3x3 rectangle with x, y as top-left
+  def draw_rect(x, y)
+    MegaMrbc.draw_top_left(x, y)
+    MegaMrbc.draw_top_centre(x + 1, y)
+    MegaMrbc.draw_top_right(x + 2, y)
+    MegaMrbc.draw_left(x, y + 1)
+    MegaMrbc.draw_right(x + 2, y + 1)
+    MegaMrbc.draw_bottom_left(x, y + 2)
+    MegaMrbc.draw_bottom_centre(x + 1, y + 2)
+    MegaMrbc.draw_bottom_right(x + 2, y + 2)
+  end
+
+  def draw_kb(x, y)
+    [0, 1, 2].each do |i|
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].each do |j|
+        draw_rect(x + j * 3, y + i * 3) unless i == 2 && j > 7
+        MegaMrbc.draw_text(KEYBOARD[i][j], x + j * 3 + 1, y + i * 3 + 1)
+      end
+    end
   end
 end
 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,169 @@
 #include "mrubyc.h"
 #include <stdint.h>
 #include <genesis.h>
+#include <bmp.h> // drawline
 
 #define int8_t s8
 
 typedef char s8;
+
+// Sprites for rectangles
+
+enum rect_parts { tl, tc, tr, l, c, r, bl, bc, br };
+
+const u32 top_left[8] =
+  {
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00011111,
+    0x00011111,
+    0x00011000,
+    0x00011000,
+    0x00011000
+  };
+
+const u32 top_centre[8] =
+  {
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x11111111,
+    0x11111111,
+    0x00000000,
+    0x00000000,
+    0x00000000
+  };
+
+const u32 top_right[8] =
+  {
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x11111000,
+    0x11111000,
+    0x00011000,
+    0x00011000,
+    0x00011000
+  };
+
+const u32 left[8] =
+  {
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011000
+  };
+
+const u32 centre[8] =
+  {
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000
+  };
+
+const u32 *right = left;
+
+const u32 bottom_left[8] =
+  {
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x00011111,
+    0x00011111,
+    0x00000000,
+    0x00000000,
+    0x00000000
+  };
+
+const u32 *bottom_centre = top_centre;
+
+const u32 bottom_right[8] =
+  {
+    0x00011000,
+    0x00011000,
+    0x00011000,
+    0x11111000,
+    0x11111000,
+    0x00000000,
+    0x00000000,
+    0x00000000
+  };
+
+const u32 cursor[8] =
+  {
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000000,
+    0x00000055,
+    0x00000050
+  };
+
+const u32 tick[8] =
+  {
+    0x00000000,
+    0x00000011,
+    0x00000110,
+    0x00000110,
+    0x01101110,
+    0x00111100,
+    0x00011000,
+    0x00000000
+  };
+
+const u32 green[8] =
+  {
+    0x22222222,
+    0x22222222,
+    0x22222222,
+    0x22222222,
+    0x22222222,
+    0x22222222,
+    0x22222222,
+    0x22222222
+  };
+
+const u32 yellow[8] =
+  {
+    0x33333333,
+    0x33333333,
+    0x33333333,
+    0x33333333,
+    0x33333333,
+    0x33333333,
+    0x33333333,
+    0x33333333
+  };
+
+void load_tiles() {
+  VDP_loadTileData(top_left, TILE_USERINDEX + tl, 1, 0);
+  VDP_loadTileData(top_centre, TILE_USERINDEX + tc, 1, 0);
+  VDP_loadTileData(top_right, TILE_USERINDEX + tr, 1, 0);
+  VDP_loadTileData(left, TILE_USERINDEX + l, 1, 0);
+  VDP_loadTileData(centre, TILE_USERINDEX + c, 1, 0);
+  VDP_loadTileData(right, TILE_USERINDEX + r, 1, 0);
+  VDP_loadTileData(bottom_left, TILE_USERINDEX + bl, 1, 0);
+  VDP_loadTileData(bottom_centre, TILE_USERINDEX + bc, 1, 0);
+  VDP_loadTileData(bottom_right, TILE_USERINDEX + br, 1, 0);
+
+  // this one is after the parts...
+  VDP_loadTileData( cursor, TILE_USERINDEX + br + 1, 1, 0);
+  VDP_loadTileData( tick, TILE_USERINDEX + br + 2, 1, 0);
+  VDP_loadTileData( green, TILE_USERINDEX + br + 3, 1, 0);
+  VDP_loadTileData( yellow, TILE_USERINDEX + br + 4, 1, 0);
+}
 
 static void c_megamrbc_draw_text(mrb_vm *vm, mrb_value *v, int argc)
 {
@@ -14,6 +173,62 @@ static void c_megamrbc_draw_text(mrb_vm *vm, mrb_value *v, int argc)
   char buf[32];
   snprintf(buf, 31, "%s", ptr);
   VDP_drawText(buf, x, y);
+}
+
+static void c_megamrbc_draw_top_left(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+tl, x, y);
+}
+
+static void c_megamrbc_draw_top_centre(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+tc, x, y);
+}
+
+static void c_megamrbc_draw_top_right(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+tr, x, y);
+}
+
+static void c_megamrbc_draw_left(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+l, x, y);
+}
+
+static void c_megamrbc_draw_right(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+r, x, y);
+}
+
+static void c_megamrbc_draw_bottom_left(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+bl, x, y);
+}
+
+static void c_megamrbc_draw_bottom_centre(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+bc, x, y);
+}
+
+static void c_megamrbc_draw_bottom_right(mrb_vm *vm, mrb_value *v, int argc)
+{
+  char x = mrbc_integer(v[1]);
+  char y = mrbc_integer(v[2]);
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+br, x, y);
 }
 
 static void c_megamrbc_read_joypad(mrb_vm *vm, mrb_value *v, int argc) {
@@ -26,12 +241,60 @@ static void c_megamrbc_wait_vblank(mrb_vm *vm, mrb_value *v, int argc) {
   SYS_doVBlankProcess();
 }
 
+static void c_megamrbc_show_cursor(mrb_vm *vm, mrb_value *v, int argc) {
+  uint8_t x = mrbc_integer(v[1]);
+  uint8_t y = mrbc_integer(v[2]);
+  uint16_t x_px = x * 8;
+  uint16_t y_px = y * 8;
+  VDP_setSpriteFull(0, x_px, y_px, SPRITE_SIZE(1,1), TILE_ATTR_FULL(0,1,0,0,TILE_USERINDEX + br + 1), 1);
+  VDP_setSpriteFull(1, x_px+16, y_px, SPRITE_SIZE(1,1), TILE_ATTR_FULL(0,1,0,1,TILE_USERINDEX + br + 1), 2);
+  VDP_setSpriteFull(2, x_px, y_px+16, SPRITE_SIZE(1,1), TILE_ATTR_FULL(0,1,1,0,TILE_USERINDEX + br + 1), 3);
+  VDP_setSprite(3, x_px+16, y_px+16, SPRITE_SIZE(1,1), TILE_ATTR_FULL(0,1,1,1,TILE_USERINDEX + br + 1));
+  VDP_updateSprites(4, 1);
+}
+
+static void c_megamrbc_show_tick(mrb_vm *vm, mrb_value *v, int argc) {
+  uint8_t x = mrbc_integer(v[1]);
+  uint8_t y = mrbc_integer(v[2]);
+  uint16_t x_px = (x + 1) * 8;
+  uint16_t y_px = (y + 1) * 8;
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+br+2, x, y);
+}
+
+static void c_megamrbc_draw_green_square(mrb_vm *vm, mrb_value *v, int argc) {
+  uint8_t x = mrbc_integer(v[1]);
+  uint8_t y = mrbc_integer(v[2]);
+  uint16_t x_px = (x + 1) * 8;
+  uint16_t y_px = (y + 1) * 8;
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+br+3, x, y);
+}
+
+static void c_megamrbc_draw_yellow_square(mrb_vm *vm, mrb_value *v, int argc) {
+  uint8_t x = mrbc_integer(v[1]);
+  uint8_t y = mrbc_integer(v[2]);
+  uint16_t x_px = (x + 1) * 8;
+  uint16_t y_px = (y + 1) * 8;
+  VDP_setTileMapXY(BG_B, TILE_USERINDEX+br+4, x, y);
+}
+
 void make_class(mrb_vm *vm)
 {
   mrb_class *cls = mrbc_define_class(vm, "MegaMrbc", mrbc_class_object);
   mrbc_define_method(vm, cls, "draw_text", c_megamrbc_draw_text);
+  mrbc_define_method(vm, cls, "draw_top_left", c_megamrbc_draw_top_left);
+  mrbc_define_method(vm, cls, "draw_top_centre", c_megamrbc_draw_top_centre);
+  mrbc_define_method(vm, cls, "draw_top_right", c_megamrbc_draw_top_right);
+  mrbc_define_method(vm, cls, "draw_left", c_megamrbc_draw_left);
+  mrbc_define_method(vm, cls, "draw_right", c_megamrbc_draw_right);
+  mrbc_define_method(vm, cls, "draw_bottom_left", c_megamrbc_draw_bottom_left);
+  mrbc_define_method(vm, cls, "draw_bottom_centre", c_megamrbc_draw_bottom_centre);
+  mrbc_define_method(vm, cls, "draw_bottom_right", c_megamrbc_draw_bottom_right);
   mrbc_define_method(vm, cls, "read_joypad", c_megamrbc_read_joypad);
   mrbc_define_method(vm, cls, "wait_vblank", c_megamrbc_wait_vblank);
+  mrbc_define_method(vm, cls, "show_cursor", c_megamrbc_show_cursor);
+  mrbc_define_method(vm, cls, "show_tick", c_megamrbc_show_tick);
+  mrbc_define_method(vm, cls, "draw_green_square", c_megamrbc_draw_green_square);
+  mrbc_define_method(vm, cls, "draw_yellow_square", c_megamrbc_draw_yellow_square);
 }
 
 void mrubyc(uint8_t *mrbbuf)
@@ -59,30 +322,30 @@ void mrubyc(uint8_t *mrbbuf)
 
 extern const uint8_t mrbsrc[];
 
+void set_up_colours() {
+  VDP_setPaletteColor(1, RGB24_TO_VDPCOLOR(0x222222)); // dark grey
+  VDP_setPaletteColor(2, RGB24_TO_VDPCOLOR(0x6AAA64)); // green
+  VDP_setPaletteColor(3, RGB24_TO_VDPCOLOR(0xC9B458)); // yellow
+  VDP_setPaletteColor(4, RGB24_TO_VDPCOLOR(0x787C7E)); // grey
+  VDP_setPaletteColor(5, RGB24_TO_VDPCOLOR(0x2222AA)); // blue
+}
+
 int main(void) {
+  // Initialise screen
   VDP_setScreenWidth320();
   VDP_setHInterrupt(0);
   VDP_setHilightShadow(0);
-  PAL_setColor(15+16, 0x0222);
+  PAL_setColor(15, 0x0222); // default text colour
   VDP_setTextPalette(0);
-  PAL_setColor(0, 0x0882);
+  PAL_setColor(0, 0x0EEE);
+  VDP_resetSprites();
+
+  set_up_colours();
+  load_tiles();
 
   if( mrbsrc == 0 ) return 1;
 
   mrubyc( mrbsrc );
-
-  uint16_t i, value;
-  while(TRUE)
-  {
-
-    for(i=0; i < 2 ; i ++) {
-      value = JOY_readJoypad(i);
-      char buf[19];
-      snprintf(buf, 18, "Joypad state: %03d", value);
-      VDP_drawText(buf, 8, 10+i);
-    }
-    SYS_doVBlankProcess();
-  }
 
   return 0;
 }
